8e27884cffa2a15ff1720d90128d8ea547f901f6
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-16 08:27:55 -0700

    Update first test spec to include outputting false if the two phrases are not exactly the same

--- spec/ana_anti_gram_spec.rb
+++ spec/ana_anti_gram_spec.rb
@@ -3,7 +3,8 @@ require('ana_anti_gram')
 require('pry')
 
 describe('ana_anti_gram?') do
-  it("will return true if the two arguments are the same ") do
+  it("will return the appropriate boolean case if the two arguments are the same or not ") do
     expect(ana_anti_gram?("hello", "hello")).to(eq(true))
+    expect(ana_anti_gram?("hello", "ello")).to(eq(false))
   end
 end
